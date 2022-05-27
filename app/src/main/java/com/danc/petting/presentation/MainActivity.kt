2f26affbab175300d95f9574f8c59baf4048f2b8
Danc-0 <lunjaludaniel@gmail.com>
2022-05-27 22:58:20 +0300

    Splash Screen Design

--- app/src/main/java/com/danc/petting/presentation/MainActivity.kt
+++ app/src/main/java/com/danc/petting/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.danc.petting.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.danc.petting.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
